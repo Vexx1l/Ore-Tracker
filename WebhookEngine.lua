faf4790f1d35b384b1e95f5daec437f178681227
Vexx1l <stromcutter11@gmail.com>
2026-01-13 14:41:14 -0500

    Update WebhookEngine.lua

--- WebhookEngine.lua
+++ WebhookEngine.lua
@@ -1,1 +1,12 @@
-
+return {
+    WebhookURL = "YOUR_WEBHOOK_HERE",
+    DiscordID = "YOUR_ID_HERE", -- To ping you for rare finds
+    NotifyRarities = {
+        ["Legendary"] = true,
+        ["Mythical"] = true,
+        ["Divine"] = true,
+        ["Relic"] = true
+    },
+    AutoPing = true,
+    MinMaxState = "Open" -- Default UI state
+}
